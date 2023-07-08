1ff312b13b162c9c21fd2356b2f369c4ded3b4e5
Christoph Friedrich <christoph@christophfriedrich.de>
2023-07-08 21:55:23 +0200

    Implemented JS function to show a notice inside the admin backend

--- js/admin/notice.js
+++ js/admin/notice.js
@@ -1,8 +1,44 @@
 import $ from 'jquery';
 
-export default class Notice {
-    showNotice()
-    {
-        alert('I AM SHOWING NOTICE');
+export const NOTICE_TYPE = {
+    ERROR: 'notice-error',
+    WARNING: 'notice-warning',
+    SUCCESS: 'notice-success',
+    INFO: 'notice-info'
+};
+
+export function showNotice(content, type = NOTICE_TYPE.INFO, isDismissible = true)
+{
+    if (Object.values(NOTICE_TYPE).indexOf(type) === -1) {
+        console.error('Notice type [' + type + '] not supported!');
+        return;
+    }
+
+    const elContent = $('<p>' + content + '</p>');
+    const elOuterDiv = $('<div>');
+    elOuterDiv.addClass('notice');
+    elOuterDiv.addClass(type);
+
+    if (isDismissible) {
+        elOuterDiv.addClass('is-dismissible');
+    }
+
+    elOuterDiv.append(elContent);
+
+    console.log("notice", elOuterDiv);
+
+    const noticeList = $('.notice');
+    console.log("NOTICE LIST", noticeList);
+    if (noticeList.length === 0) {
+        // No notice is there. So we insert it as the first child into #wpbody-content
+        const elWpBodyContent = $('#wpbody-content');
+        if (!elWpBodyContent) {
+            console.error('<div> with id #wpbody-content not found! Are you inside admin backend?');
+            return;
+        }
+
+        elWpBodyContent.prepend(elOuterDiv);
+    } else {
+        noticeList.last().after(elOuterDiv);
     }
 }
