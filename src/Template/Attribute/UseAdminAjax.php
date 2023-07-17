6b1845980b94f4b794213f29864dddd14b8ff485
Christoph Friedrich <christoph@christophfriedrich.de>
2023-07-17 11:22:10 +0200

    Adding repeatable attribute to UseAdminAjax

--- src/Template/Attribute/UseAdminAjax.php
+++ src/Template/Attribute/UseAdminAjax.php
@@ -4,7 +4,7 @@ namespace VAF\WP\Framework\Template\Attribute;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_CLASS)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 class UseAdminAjax
 {
     public function __construct(public readonly string $actionName)
