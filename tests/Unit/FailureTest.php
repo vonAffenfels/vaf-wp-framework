eddc924c252ffe565f491da7cb2415b646d35582
speckmaier <sven.speckmaier@vonaffenfels.de>
2023-12-21 11:25:34 +0100

    add failing test to test teams integration

---
+++ tests/Unit/FailureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VAF\WP\FrameworkTests\Unit;
+
+class FailureTest
+{
+
+    /**
+     * @test
+     */
+    public function should_fail_to_test_teams_integration()
+    {
+        $this->assertTrue(false);
+    }
+
+}
